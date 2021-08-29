7d0e2afb5cb65c5abf017ad736916fbdbe61e451
rem0930 <83850299+rem0930@users.noreply.github.com>
2021-08-29 12:44:14 +0900

    カレンダー

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,6 @@ from app import views
 urlpatterns = [
     path('', views.StoreView.as_view(), name='store'),
     path('store/<int:pk>/', views.StaffView.as_view(), name='staff'), # 追加
+    path('calendar/<int:pk>/', views.CalendarView.as_view(), name='calendar'), # 追加
+    path('calendar/<int:pk>/<int:year>/<int:month>/<int:day>/', views.CalendarView.as_view(), name='calendar'), # 追加
 ]
