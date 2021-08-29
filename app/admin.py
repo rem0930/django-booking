951a3d227060437b05e252bec3976f1089b6cc85
rem0930 <83850299+rem0930@users.noreply.github.com>
2021-08-29 11:59:14 +0900

    スタッフリスト

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Store, Staff
 
+admin.site.register(Store)
+admin.site.register(Staff)
 # Register your models here.
--- app/views.py
+++ app/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin
-# Create your views here.
-from django.views.generic import TemplateView
+from app.models import Store, Staff
+from django.views.generic import View
+from django.shortcuts import get_object_or_404, render
 
 
-class IndexView(LoginRequiredMixin, TemplateView):
-    template_name = "app/index.html"
-    login_url = '/accounts/login/'
+class StoreView(View):
+    def get(self, request, *args, **kwargs):
+        store_data = Store.objects.all()
+
+        return render(request, 'app/store.html', {
+            'store_data': store_data,
+        }
+
+class StaffView(View):
+    def get(self, request, *args, **kwargs):
+        store_data = get_object_or_404(Store, id=self.kwargs['pk'])
+        staff_data = Staff.objects.filter(store=store_data).select_related('user')
+
+        return render(request, 'app/staff.html', {
+            'store_data': store_data,
+            'staff_data': staff_data,
+        })
